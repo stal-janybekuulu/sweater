405f11db4e44ec215dbf987c8e5287de2edab919
stal <stal.janybekuulu@gmail.com>
2019-06-23 01:52:03 +0600

    Initial commit

---
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -0,0 +1,33 @@
+package com.example.sweater.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity //Сущность таблицы в БД
+public class Message {
+
+    @Id //Говорим, что это идентификатор
+    @GeneratedValue(strategy= GenerationType.AUTO) //Генерация идентификаторов
+    private Integer id;
+
+    private String text;
+    private String tag;
+
+    public Message(String text, String tag) {
+        this.text = text;
+        this.tag = tag;
+    }
+
+    public Message() {
+    }
+
+}
